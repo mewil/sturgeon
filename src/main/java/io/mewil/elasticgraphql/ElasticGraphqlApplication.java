84037172131aa9f19c9849f60685eafc2155eda0
Michael Wilson <mewil@umich.edu>
2020-11-09 21:27:54 -0800

    :tada: Initial Commit
    Create a Spring-based gradle project.

---
+++ src/main/java/io/mewil/elasticgraphql/ElasticGraphqlApplication.java
@@ -0,0 +1,13 @@
+package io.mewil.elasticgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticGraphqlApplication.class, args);
+	}
+
+}
